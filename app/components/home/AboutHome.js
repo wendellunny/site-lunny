48c8ff476d28e9c98c28d09ed27b89b0f8cf3854
wendellunny <wendelferreiraportugal@gmail.com>
2021-09-02 01:33:26 -0300

    Color

--- app/components/home/AboutHome.js
+++ app/components/home/AboutHome.js
@@ -1,14 +1,17 @@
+import styles from '../../../styles/Home.module.css';
+
 export function AboutHome(){
     return(
         <div>
             <p><span>1</span>//Olá Mundo;</p>
             <p><span>2</span>//Eu sou Wendel Luiz</p>
-            <p><span>4</span>{'if(dia){'}</p>
-            <p><span>5</span> &nbsp; darBomdDia();</p>
-            <p><span>6</span>{'}else{'}</p>
-            <p><span>7</span> &nbsp; darBoaNoite();</p>
+            <p className={styles.if}><span>4</span>{'if(dia){'}</p>
+            <p className={styles.function}><span>5</span> &nbsp; darBomDia</p>
+            <p className={styles.if}><span>6</span>{'}else{'}</p>
+            <p className={styles.function}><span>7</span> &nbsp; darBoaNoite</p>
             <p><span>8</span>}</p>
 
         </div>
     )
 }
+
